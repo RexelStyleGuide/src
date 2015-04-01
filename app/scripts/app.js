c08ffc3832b35b880507d2aef8522517d912119a
Rich Sollenne <RichSollenne@Richs-MacBook-Pro.local>
2015-04-01 16:13:47 -0500

    project framework

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+//use foundation
+$(document).foundation();
+
+/**
+ * @ngdoc overview
+ * @name srcApp
+ * @description
+ * # srcApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('srcApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
