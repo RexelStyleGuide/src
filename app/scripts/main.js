d79339b198ec24b6af98642be1b0b3900bced9e2
Rich Sollenne <RichSollenne@Richs-MacBook-Pro.local>
2015-04-07 16:11:35 -0500

    mostly finished side nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,8 +6,23 @@ $(document).ready(function() {
 		$('.inner-wrap').toggleClass('sidenav-open');
 	});
 
-	$('.off-canvas-list > li > a').click(function() {
+	$('.off-canvas-list li a').click(function() {
 		$(this).addClass('active').parent().siblings().find('a').removeClass('active');
 	});
 
+	var on = false;
+	function dropdownCaret(element) {
+		var on = element.hasClass('slideOpen') ? true : false;
+		var animateCaret = on ? element.find('#shape-open')[0] : element.find('#shape-close')[0];
+		animateCaret.beginElement();
+	}
+
+	$('.top-level-dropdown').click(function() {
+		$(this).toggleClass('slideOpen').parent().next('li').slideToggle();
+		dropdownCaret($(this));
+	});
+
+
 });
+
+
