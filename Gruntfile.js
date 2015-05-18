e94f283e4d491bf1239bbb4a469c6a444191de14
Rich Sollenne <RichSollenne@Richs-MacBook-Pro.local>
2015-05-18 13:34:36 -0500

    fixing grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -216,7 +216,7 @@ module.exports = function (grunt) {
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
-        relativeAssets: false,
+        relativeAssets: true,
         assetCacheBuster: false,
         raw: 'Sass::Script::Number.precision = 10\n'
       },
@@ -402,7 +402,7 @@ module.exports = function (grunt) {
         'compass'
       ],
       dist: [
-       // 'compass:dist'
+        'compass:dist',
         'imagemin',
         'svgmin'
       ]
