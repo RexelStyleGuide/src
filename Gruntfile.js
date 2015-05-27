efa80828d336dd716e3596dd8b0f3dd457a5135e
Rich Sollenne <RichSollenne@Richs-MacBook-Pro.local>
2015-05-27 15:18:36 -0500

    update gruntfile for font dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -239,7 +239,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          '<%= yeoman.dist %>/font/*'
         ]
       }
     },
@@ -376,7 +376,7 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'font/{,*/}*.*'
           ]
         }, {
           expand: true,
